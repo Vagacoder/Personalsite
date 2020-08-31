918d177405ceca2d517980d52aa00537c994bbcb
Vagacoder <qiruihu@gmail.com>
2020-08-30 18:39:26 -0700

    8/30 contact skeleton is 80% done

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -9,12 +9,11 @@ const Contact = React.forwardRef((props: any, ref: any) => {
   return (
     <section id="contact" ref={ref}>
       <div>
-        This is Contact
-        <MUI.Grid container>
-          <MUI.Grid item xs={4}>
+        <MUI.Grid container className="ContactContainer">
+          <MUI.Grid item xs={12} sm={4} className="LeftColumn">
             <FontAwesomeIcon icon={faCommentDots} className="MailIcon" />
           </MUI.Grid>
-          <MUI.Grid item xs={8} className="Title">
+          <MUI.Grid item xs={12} sm={8} className="Title RightColumn">
             <MUI.Typography variant="h6">
               This page is currently under construction.
             </MUI.Typography>
@@ -24,14 +23,47 @@ const Contact = React.forwardRef((props: any, ref: any) => {
           </MUI.Grid>
 
           <MUI.Grid container className="ContactTable">
-            <MUI.Grid item xs={4}>
+            <MUI.Grid item xs={4} className="LeftColumn">
               <MUI.Typography variant="h6">
-                Name *:
+                Name *
               </MUI.Typography>
             </MUI.Grid>
-            <MUI.Grid item xs={8}>
+            <MUI.Grid item xs={8} className="RightColumn">
               <MUI.TextField required id="SenderName" placeholder="Your name" variant="outlined" />
             </MUI.Grid>
+
+            <MUI.Grid item xs={4} className="LeftColumn">
+              <MUI.Typography variant="h6">
+                Email *
+              </MUI.Typography>
+            </MUI.Grid>
+            <MUI.Grid item xs={8} className="RightColumn">
+              <MUI.TextField required id="SenderEmail" placeholder="Your Email" variant="outlined" />
+            </MUI.Grid>
+
+            <MUI.Grid item xs={4} className="LeftColumn">
+              <MUI.Typography variant="h6">
+                Subject
+              </MUI.Typography>
+            </MUI.Grid>
+            <MUI.Grid item xs={8} className="RightColumn">
+              <MUI.TextField id="Subject" variant="outlined" />
+            </MUI.Grid>
+
+            <MUI.Grid item xs={4} className="LeftColumn">
+              <MUI.Typography variant="h6">
+                Message *
+              </MUI.Typography>
+            </MUI.Grid>
+            <MUI.Grid item xs={8} className="RightColumn">
+              <MUI.TextField required multiline id="SenderName" variant="outlined" />
+            </MUI.Grid>
+
+            <MUI.Grid item xs={4} className="LeftColumn"></MUI.Grid>
+            <MUI.Grid item xs={8} className="RightColumn">
+              <MUI.Button>Submit</MUI.Button>
+            </MUI.Grid>
+
           </MUI.Grid>
         </MUI.Grid>
       </div>
